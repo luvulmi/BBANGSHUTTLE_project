855bd1467c2fff2651a80a7a604c33635867c5dd
Kim Yurim <110047935+luvulmi@users.noreply.github.com>
2022-11-14 20:59:43 +0900

    아아아아ㅏ아아ㅏㅇ

--- bbang_homepage/mypage/mypage.js
+++ bbang_homepage/mypage/mypage.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// ================ 날짜 설정 ================
 const date = new Date(),
   nowYear = date.getFullYear(),
   nowDay = date.getDate();
@@ -45,7 +45,7 @@ $input[1].value = getDate(date);
 
 // 최근 날짜 버튼 클릭 시
 let preBtn = dateBtn[0];
-  
+
 
 bx_term.addEventListener('click', function (e) {
   const eBtn = e.target;
@@ -59,3 +59,34 @@ bx_term.addEventListener('click', function (e) {
     preBtn = eBtn;
   }
 });
+
+// ================= 쿠폰 탭 ===============
+const couponTab = document.querySelector('.coupon_tab'),
+  couponBtn = couponTab.querySelectorAll('.coupon_tab a'),
+  useCoupon = document.querySelector('.use_coupon'),
+  useCoupon1 = document.getElementById('usable_coupon');
+
+let precouponBtn = couponBtn[0],
+  preContent = useCoupon1;
+
+// 버튼 클릭 시 동작할 함수
+function Btn_click(e) {
+  e.preventDefault();
+  let currentContent = useCoupon.querySelector(e.target.getAttribute('href'));
+
+  if (e.target.hasAttribute('href')) {
+    precouponBtn.parentNode.classList.remove('active');
+    e.target.parentNode.classList.add('active');
+
+    preContent.style.display = "none";
+    currentContent.style.display = "block";
+
+    precouponBtn = e.target;
+    preContent = currentContent;
+  }
+}
+
+// 첫번째 내용 보이기
+useCoupon1.style.display = "block";
+// 클릭 이벤트
+couponTab.addEventListener('click', Btn_click);
---
+++ bbang_homepage/mypage/mypage_review.js
@@ -0,0 +1,30 @@
+// ============== 리뷰 탭 ===============
+const reviewTab = document.querySelector('.review_tab'),
+    reviewBtn = reviewTab.querySelectorAll('.review_tab a'),
+    writeReview = document.querySelector('.write_review'),
+    unReview = document.getElementById('un_review');
+
+let prereviewBtn = reviewBtn[0],
+    preContent1 = unReview;
+
+// 버튼 클릭 시 동작할 함수
+function Btn_click2(e) {
+    e.preventDefault();
+    let currentContent = writeReview.querySelector(e.target.getAttribute('href'));
+
+    if (e.target.hasAttribute('href')) {
+        prereviewBtn.parentNode.classList.remove('active');
+        e.target.parentNode.classList.add('active');
+
+        preContent1.style.display = "none";
+        currentContent.style.display = "block";
+
+        prereviewBtn = e.target;
+        preContent1 = currentContent;
+    }
+}
+
+// 첫번째 내용 보이기
+unReview.style.display = "block";
+// 클릭 이벤트
+reviewTab.addEventListener('click', Btn_click2);
